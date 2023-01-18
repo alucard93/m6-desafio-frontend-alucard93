11cb26d5809fc9c3f33f5ad42683b1d1b4383096
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-01-17 22:05:13 -0300

    feat: Dashboard
    criação da page Dashboard

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledDashboard = styled.div`
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+  
+    .border-dashboard{
+      border: 4px solid var(--borderForm);
+      border-radius: 1rem;
+    }
+
+  h1 {
+    font-size: 24px;
+    text-size-adjust: initial;
+    color: var(--textH1);
+  }
+`;
